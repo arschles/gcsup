fde5efc19ae23e495dd8827fd1b2bd929edbcf82
Aaron Schlesinger <arschles@gmail.com>
2015-12-12 16:57:34 -0800

    version env var

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,16 +5,16 @@
 
 cd "$(dirname "$0")" || exit 1
 
-VER=0.0.1
+VERSION=0.0.1
 # note that we must check TRAVIS_PULL_REQUEST first because, for pull request builds, TRAVIS_BRANCH will contain the name of the branch that the PR is _targeting_ (not the name of the branch it is trying to merge).
 # see https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables for an explanation of TRAVIS_PULL_REQUEST and TRAVIS_BRANCH
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  VER="pr-$TRAVIS_PULL_REQUEST"
+  VERSION="pr-$TRAVIS_PULL_REQUEST"
 elif  [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  VER="branch-$TRAVIS_BRANCH"
+  VERSION="branch-$TRAVIS_BRANCH"
 fi
 
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-export IMAGE_NAME="quay.io/arschles/gcsup:$VER"
+export IMAGE_NAME="quay.io/arschles/gcsup:$VERSION"
 echo "building and pushing $IMAGE_NAME"
 make build docker-build docker-push
