a04f153e4259c7dd1137b5530ee13a30211c240f
Aaron Schlesinger <arschles@gmail.com>
2016-01-17 15:34:10 -0800

    add quay creds

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,4 +4,5 @@
 #
 # This script was adapted from https://github.com/deis/builder
 
+docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 make -C .. build docker-build docker-push
